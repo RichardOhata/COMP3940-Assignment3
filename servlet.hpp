9138860e717c1c51973a8cb97ccdf2da1b4de950
RichardOhata <richardohata@gmail.com>
2022-10-24 14:52:11 -0700

    Initial Commits

---
+++ servlet.hpp
@@ -0,0 +1,12 @@
+//
+// Created by richa on 2022-10-24.
+//
+
+#ifndef COMP3940_ASSIGNMENT3_SERVLET_HPP
+#define COMP3940_ASSIGNMENT3_SERVLET_HPP
+
+class servlet {
+    virtual void doGet() = 0;
+    virtual void doPost() = 0;
+};
+#endif //COMP3940_ASSIGNMENT3_SERVLET_HPP
